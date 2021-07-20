b1c6ec2767d6b13d2cecaf931fa087ff25fcbfba
Itachi <1241928215@qq.com>
2021-07-20 16:22:31 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第一次修改");
+        System.out.println("第三次修改");
     }
 }
