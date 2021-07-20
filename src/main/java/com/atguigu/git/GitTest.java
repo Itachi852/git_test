7cca885261a0ba468f0efcfc734efcaa5d28cf66
Itachi <1241928215@qq.com>
2021-07-20 17:22:11 +0800

    push test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("第一次修改");
         System.out.println("第三次修改");
         System.out.println("fix-hot修改");
+        System.out.println("push-hot修改");
     }
 }
